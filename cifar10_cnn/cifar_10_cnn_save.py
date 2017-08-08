672108017e6fc18edb407276b3da06eddb0b802e
朱新辉 <zhuxinhui@imzhiliao.com>
2017-08-08 12:23:11 +0800

    update

--- cifar10_cnn/cifar_10_cnn_save.py
+++ cifar10_cnn/cifar_10_cnn_save.py
@@ -14,15 +14,18 @@ import numpy as np
 work_dir = '/tmp/cifar10_cnn/1'
 
 
-# max_steps = 20000
-# batch_size = 128
-max_steps = 1
-batch_size = 1
+max_steps = 20000
+batch_size = 128
+# max_steps = 1a
+# batch_size = 1
+KEEPB = 0.5
 data_dir = '/Users/zhuxinhui/work/MLPro/tf-test/cifar10_data/cifar-10-batches-bin'
 
 
-def variable_width_weight_loss(shape, stddev, wl):
-    var = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+def variable_width_weight_loss(shape, stddev, wl, name):
+    # var = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+    init = tf.truncated_normal(shape, stddev=stddev)
+    var = tf.get_variable(initializer=init, name=name)
     if wl is not None:
         weight_loss = tf.multiply(tf.nn.l2_loss(var), wl, name='weight_loss')
         tf.add_to_collection('losses', weight_loss)
@@ -47,10 +50,12 @@ class CIFAR_CNN(object):
 
         with tf.variable_scope('w_b_variable', reuse=reuseFg):
             self.weight1 = variable_width_weight_loss(
-                shape=[5, 5, 3, 64], stddev=5e-2, wl=0.0)
+                shape=[5, 5, 3, 64], stddev=5e-2, wl=0.0, name='vwwl1')
             self.kernel1 = tf.nn.conv2d(self.image_holder, self.weight1, [
                 1, 1, 1, 1], padding='SAME')
-            self.bias1 = tf.Variable(tf.constant(0.0, shape=[64]))
+            # self.bias1 = tf.Variable(tf.constant(0.0, shape=[64]))
+            self.bias1 = tf.get_variable(
+                initializer=tf.constant(0.0, shape=[64]), name='bias1')
             self.conv1 = tf.nn.relu(tf.nn.bias_add(self.kernel1, self.bias1))
             self.pool1 = tf.nn.max_pool(self.conv1, ksize=[1, 3, 3, 1], strides=[
                 1, 2, 2, 1], padding='SAME')
@@ -58,10 +63,12 @@ class CIFAR_CNN(object):
                                    alpha=0.001 / 9.0, beta=0.75)
 
             self.weight2 = variable_width_weight_loss(
-                shape=[5, 5, 64, 64], stddev=5e-2, wl=0.0)
+                shape=[5, 5, 64, 64], stddev=5e-2, wl=0.0, name='vwwl2')
             self.kernel2 = tf.nn.conv2d(self.norm1, self.weight2, [
                                         1, 1, 1, 1], padding='SAME')
-            self.bias2 = tf.Variable(tf.constant(0.1, shape=[64]))
+            # self.bias2 = tf.Variable(tf.constant(0.1, shape=[64]))
+            self.bias2 = tf.get_variable(
+                initializer=tf.constant(0.1, shape=[64]), name='bias2')
             self.conv2 = tf.nn.relu(tf.nn.bias_add(self.kernel2, self.bias2))
             self.norm2 = tf.nn.lrn(self.conv2, 4, bias=1.0,
                                    alpha=0.001 / 9.0, beta=0.75)
@@ -71,20 +78,28 @@ class CIFAR_CNN(object):
             self.reshape = tf.reshape(self.pool2, [batch_size, -1])
             self.dim = self.reshape.get_shape()[1].value
             self.weight3 = variable_width_weight_loss(
-                shape=[self.dim, 384], stddev=0.04, wl=0.004)
-            self.bias3 = tf.Variable(tf.constant(0.1, shape=[384]))
+                shape=[self.dim, 384], stddev=0.04, wl=0.004, name='vwwl3')
+            # self.bias3 = tf.Variable(tf.constant(0.1, shape=[384]))
+            self.bias3 = tf.get_variable(
+                initializer=tf.constant(0.1, shape=[384]), name='bias3')
             self.local3 = tf.nn.relu(
                 tf.matmul(self.reshape, self.weight3) + self.bias3)
+            self.local3 = tf.nn.dropout(self.local3, KEEPB)
 
             self.weight4 = variable_width_weight_loss(
-                shape=[384, 192], stddev=0.04, wl=0.004)
-            self.bias4 = tf.Variable(tf.constant(0.1, shape=[192]))
+                shape=[384, 192], stddev=0.04, wl=0.004, name='vwwl4')
+            # self.bias4 = tf.Variable(tf.constant(0.1, shape=[192]))
+            self.bias4 = tf.get_variable(
+                initializer=tf.constant(0.1, shape=[192]), name='bias4')
             self.local4 = tf.nn.relu(
                 tf.matmul(self.local3, self.weight4) + self.bias4)
+            self.local4 = tf.nn.dropout(self.local4, KEEPB)
 
             self.weight5 = variable_width_weight_loss(
-                shape=[192, 10], stddev=1 / 192.0, wl=0.0)
-            self.bias5 = tf.Variable(tf.constant(0.0, shape=[10]))
+                shape=[192, 10], stddev=1 / 192.0, wl=0.0, name='vwwl5')
+            # self.bias5 = tf.Variable(tf.constant(0.0, shape=[10]))
+            self.bias5 = tf.get_variable(
+                initializer=tf.constant(0.0, shape=[10]), name='bias5')
             self.logits = tf.add(
                 tf.matmul(self.local4, self.weight5), self.bias5)
 
@@ -97,7 +112,7 @@ class CIFAR_CNN(object):
         tf.add_to_collection('losses', cross_entropy_mean)
         return tf.add_n(tf.get_collection('losses'), name='total_loss')
 
-model = CIFAR_CNN(False)
+model = CIFAR_CNN('')
 model.cost = model.loss(model.logits, model.label_holder)
 model.train_op = tf.train.AdamOptimizer(1e-3).minimize(model.cost)
 model.top_k_op = tf.nn.in_top_k(model.logits, model.label_holder, 1)
@@ -114,7 +129,7 @@ for step in range(max_steps):
     _, loss_value = sess.run([model.train_op, model.cost], feed_dict={
                              model.image_holder: image_batch, model.label_holder: label_batch})
     # duration = time.time() - start_time
-    if step % 10000 == 0:
+    if step % 100 == 0:
         # examples_per_sec = batch_size / duration
         # sec_per_batch = float(duration)
         # format_str = ('step %d,loss=%.2f (%.1f examples/sec; %.3f sec/batch)')
@@ -141,18 +156,38 @@ for step in range(max_steps):
 # precision = true_count / float(total_sample_count)
 # print('precision @ 1 = %.3f' % precision)
 
+# raise SystemExit
+
 batch_size = 1
+KEEPB = 1
+images_test, labels_test = cifar10_input.inputs(eval_data=True,
+                                                data_dir=data_dir, batch_size=batch_size)
+model = CIFAR_CNN(True)
+model.top_k_op = tf.nn.in_top_k(model.logits, model.label_holder, 5)
+saver = tf.train.Saver()
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+tf.train.start_queue_runners()
+
+module_file = tf.train.latest_checkpoint('./save/')
+saver.restore(sess, module_file)
+
+image_batch, label_batch = sess.run([images_test, labels_test])
+print(image_batch.shape, label_batch.shape)
+logits, predictions = sess.run([model.logits, model.top_k_op], feed_dict={
+    model.image_holder: image_batch, model.label_holder: label_batch})
+print(logits, predictions)
 
 
-raise SystemExit
+# raise SystemExit
 # 导出模型
 print('Exporting trained model to', work_dir)
 builder = saved_model_builder.SavedModelBuilder(work_dir)
 
 # Build the signature_def_map.
 
-tensor_info_x = utils.build_tensor_info(image_holder)
-tensor_info_y = utils.build_tensor_info(logits)
+tensor_info_x = utils.build_tensor_info(model.image_holder)
+tensor_info_y = utils.build_tensor_info(model.logits)
 
 prediction_signature = signature_def_utils.build_signature_def(
     inputs={'req_x': tensor_info_x},
